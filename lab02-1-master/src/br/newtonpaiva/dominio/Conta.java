dceefaf21828fe73b0b751f62b8f508c2b8e6f95
12111752 <duartemairon@gmail.com>
2023-03-03 21:25:36 -0300

    feat: contrutores

---
+++ lab02-1-master/src/br/newtonpaiva/dominio/Conta.java
@@ -0,0 +1,65 @@
+
+package br.newtonpaiva.dominio;
+
+import java.util.Objects;
+
+public class Conta {
+    private Integer numero;
+    private Double saldo;
+
+    public Conta() {
+        this(null);
+    }
+
+    public Conta(Integer numero) {
+        this(numero, 0.0);
+    }
+
+    public Conta(Integer numero, Double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public Double sacar(Double valor) {
+        return 0.0;
+    }
+
+    public Double depositar(Double valor) {
+        if (valor == null || valor <= 0)
+            throw new IllegalArgumentException("Valor menor ou");
+
+        saldo += valor;
+        return saldo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
+    }
+}
+
+
